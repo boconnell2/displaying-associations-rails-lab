1922389505eeda6ec43e068430860ab912694f25
boconnell2 <bmoconnell15@gmail.com>
2020-11-18 21:52:23 -0500

    commit tree 6256e6db5a1c912132b16daa977980c5c6ebca47

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Artist.destroy_all
+
+a1 = Artist.create(name: "John")
+a2 = Artist.create(name: "Smith")
+a3 = Artist.create(name: "Jane")
+
+s1 = Song.create(title: "Funky town", artist_id: a1.id)
+s2 = Song.create(title: "Junky town", artist_id: a1.id)
+s3 = Song.create(title: "Dunky town", artist_id: a2.id)
+s4 = Song.create(title: "Skunky town", artist_id: a3.id)
